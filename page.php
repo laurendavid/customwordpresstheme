7370ac9f8afcd41fae4d23cbd2abecb77e3133f6
Lauren David <lauren@laurendavid.ca>
2016-03-03 19:39:46 -0500

    added contact form and styled contact page

--- page.php
+++ page.php
@@ -2,13 +2,14 @@
 
 <div class="main">
   <div class="wrapper">
-
     <div class="content">
+      <div class="hero2" style="background-image: url('<?php echo hackeryou_get_thumbnail_url( $post ) ?>');"></div>
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
         <h2><?php get_the_title(); ?></h2>
-        <?php the_content(); ?>
+        <?php the_content( ); ?>
+
 
       <?php endwhile; // end the loop?>
     </div> <!-- /,content -->
